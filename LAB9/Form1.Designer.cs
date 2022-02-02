323a46606eb38b5ba1cf48e7e0b1f4fd748ffa15
Roman Smal <98179242+Monseratti@users.noreply.github.com>
2022-02-02 19:20:35 +0200

    git commit

--- LAB9/Form1.Designer.cs
+++ LAB9/Form1.Designer.cs
@@ -33,6 +33,7 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // textBox1
@@ -77,11 +78,21 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(43, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(244, 21);
+            this.comboBox1.TabIndex = 5;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(318, 182);
+            this.ClientSize = new System.Drawing.Size(334, 128);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -101,6 +112,7 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
 
--- LAB9/Form1.cs
+++ LAB9/Form1.cs
@@ -9,36 +9,59 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SqlClient;
+using System.Data.Common;
 
 namespace LAB9
 {
     public partial class Form1 : Form
     {
-        SqlConnection conn = null;
+        DbConnection conn = null;
         DataTable dt = null;
+        DbProviderFactory factory = null;
         string sql = "";
         string sql1 = "";
         string cs = "";
         public Form1()
         {
             InitializeComponent();
-            cs = ConfigurationManager.ConnectionStrings["Library"].ConnectionString;
-            const string AsyncEnabled = "Asynchronous Processing=true";
-            if (!cs.Contains(AsyncEnabled))
+            dt = DbProviderFactories.GetFactoryClasses();
+            foreach (DataRow item in dt.Rows)
             {
-                cs = String.Format($"{cs}; {AsyncEnabled}");
+                comboBox1.Items.Add(item[2].ToString());
             }
-            conn = new SqlConnection(cs);
+            
         }
+        private string GetConnectionStringByProviderName(string invariantName)
+        {
+            string connStr = "";
+            try
+            {
+                ConnectionStringSettingsCollection settings = ConfigurationManager.ConnectionStrings;
+
+                foreach (ConnectionStringSettings item in settings)
+                {
+                    if (item.ProviderName == invariantName)
+                    {
+                        connStr = item.ConnectionString;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            return connStr;
+        }
+
 
         private async void button1_Click(object sender, EventArgs e)
         {
-            
+
             sql = "WAITFOR DELAY '00:00:05'; select NAME from Books";
             sql1 = "WAITFOR DELAY '00:00:05'; select CONCAT(FIRST_NAME, LAST_NAME) from AUTORS";
+            DbCommand cmd = conn.CreateCommand(); cmd.CommandText = sql;
             await conn.OpenAsync();
-            SqlCommand cmd = new SqlCommand(sql, conn);
-            SqlDataReader reader = await cmd.ExecuteReaderAsync();
+            DbDataReader reader = await cmd.ExecuteReaderAsync();
             int count1 = 0;
             while (await reader.ReadAsync())
             {
@@ -46,8 +69,8 @@ namespace LAB9
             }
             textBox1.Text = count1.ToString();
             reader.Close();
-            SqlCommand cmd1 = new SqlCommand(sql1, conn);
-            SqlDataReader reader1 = await cmd1.ExecuteReaderAsync();
+            DbCommand cmd1 = conn.CreateCommand(); cmd1.CommandText = sql1;
+            DbDataReader reader1 = await cmd1.ExecuteReaderAsync();
             int count2 = 0;
             while (await reader1.ReadAsync())
             {
@@ -55,6 +78,22 @@ namespace LAB9
             }
             textBox2.Text = count2.ToString();
             reader1.Close();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex != -1)
+            {
+                cs = GetConnectionStringByProviderName(comboBox1.SelectedItem.ToString());
+                factory = DbProviderFactories.GetFactory(comboBox1.SelectedItem.ToString());
+                const string AsyncEnabled = "Asynchronous Processing=true";
+                if (!cs.Contains(AsyncEnabled))
+                {
+                    cs = String.Format($"{cs}; {AsyncEnabled}");
+                }
+                conn = factory.CreateConnection();
+                conn.ConnectionString = cs;
+            }
         }
     }
 }
