ab9b35da4fe194a2a1b7b31baddcbd9a48f92ec2
Roman Smal <98179242+Monseratti@users.noreply.github.com>
2022-02-17 19:52:46 +0200

    git commit

---
+++ SP_LAB1/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace SP_LAB1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.listView3 = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 297);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(91, 29);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Load";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(13, 13);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(259, 278);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Tile;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // listView2
+            // 
+            this.listView2.HideSelection = false;
+            this.listView2.Location = new System.Drawing.Point(305, 13);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(233, 277);
+            this.listView2.TabIndex = 3;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            this.listView2.View = System.Windows.Forms.View.Tile;
+            // 
+            // listView3
+            // 
+            this.listView3.HideSelection = false;
+            this.listView3.Location = new System.Drawing.Point(544, 16);
+            this.listView3.Name = "listView3";
+            this.listView3.Size = new System.Drawing.Size(231, 273);
+            this.listView3.TabIndex = 4;
+            this.listView3.UseCompatibleStateImageBehavior = false;
+            this.listView3.View = System.Windows.Forms.View.Tile;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 340);
+            this.Controls.Add(this.listView3);
+            this.Controls.Add(this.listView2);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.ListView listView3;
+    }
+}
+
---
+++ SP_LAB1/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SP_LAB1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var pr = Process.GetProcesses().ToList();
+            foreach (var p in pr)
+            {
+                listView1.Items.Add($"{p.Id,-10}{p.ProcessName}");
+            }
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count != 0)
+            {
+
+                listView2.Items.Clear();
+                listView3.Items.Clear();
+                string[] tmp = listView1.SelectedItems[0].Text.Split(" ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+                var thrs = Process.GetProcessesByName(tmp[1]);
+                foreach (var thr in thrs)
+                {
+                    try
+                    {
+                        foreach (ProcessModule item in thr.Modules)
+                        {
+                            listView2.Items.Add($"{item.ModuleName}_____{item.FileName}");
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        listView2.Items.Add($"{ex.Message}");
+                    }
+                    try
+                    {
+                        foreach (ProcessThread item in thr.Threads)
+                        {
+                            listView3.Items.Add($"{item.Id}_____{item.PriorityLevel}_____{item.StartTime}");
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        listView3.Items.Add($"{ex.Message}");
+                    }
+                }
+            }
+        }
+    }
+}
