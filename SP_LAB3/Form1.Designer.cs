de15ef1dbfd76803b18da7e98763f3655f75acbb
Roman Smal <98179242+Monseratti@users.noreply.github.com>
2022-02-18 19:48:35 +0200

    git commit

---
+++ SP_LAB3/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace SP_LAB3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(294, 368);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 387);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start refresh";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(186, 390);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(95, 392);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Timer value, sec";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(312, 31);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(385, 101);
+            this.textBox1.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(312, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Process info";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(312, 138);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(65, 45);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "End process";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "notepad.exe",
+            "mspaint.exe",
+            "calc.exe"});
+            this.comboBox1.Location = new System.Drawing.Point(412, 359);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(265, 21);
+            this.comboBox1.TabIndex = 7;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(683, 360);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(24, 20);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "...";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(545, 386);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(132, 26);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Run new process";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(709, 421);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ SP_LAB3/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SP_LAB3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void ResetData()
+        {
+            listBox1.Items.Clear();
+            List<string> list = new List<string>();
+            foreach (Process p in Process.GetProcesses())
+            {
+                if (!list.Contains(p.ProcessName))
+                    list.Add(p.ProcessName);
+            }
+            foreach (var item in list.OrderBy(o => o.First()).ToList())
+            {
+                listBox1.Items.Add(item);
+            }
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            ResetData();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ResetData();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!timer1.Enabled && numericUpDown1.Value != 0)
+            {
+                timer1.Interval = (int)numericUpDown1.Value * 1000;
+                timer1.Start();
+                button1.Text = "Stop refresh";
+            }
+            else
+            {
+                button1.Text = "Start refresh";
+                timer1.Stop();
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex != -1)
+            {
+                try
+                {
+                    textBox1.Text = "";
+                    Process[] processes = Process.GetProcessesByName(listBox1.SelectedItems[0].ToString());
+                    textBox1.Text += $"Process ID - {processes[0].Id}";
+                    textBox1.AppendText(Environment.NewLine);
+                    textBox1.Text += $"Start time - {processes[0].StartTime}\n\r";
+                    textBox1.AppendText(Environment.NewLine);
+                    textBox1.Text += $"Total processor time - {processes[0].TotalProcessorTime}\n\r";
+                    textBox1.AppendText(Environment.NewLine);
+                    textBox1.Text += $"Count of threads - {processes[0].Threads.Count}\n\r";
+                    textBox1.AppendText(Environment.NewLine);
+                    textBox1.Text += $"Count of process - {processes.Length}\n\r";
+                }
+                catch (Exception)
+                { }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex != -1)
+            {
+                try
+                {
+                    Process[] processes = Process.GetProcessesByName(listBox1.SelectedItems[0].ToString());
+                    foreach (Process item in processes)
+                    {
+                        item.CloseMainWindow();
+                        item.Close();
+                    }
+                    ResetData();
+                }
+                catch (Exception)
+                { }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog opf = new OpenFileDialog();
+            opf.Filter = "Common file|*.exe";
+            if (opf.ShowDialog() == DialogResult.OK)
+            {
+                comboBox1.Items.Add(opf.FileName);
+                comboBox1.SelectedIndex = comboBox1.Items.Count - 1;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Process.Start(comboBox1.Text);
+                ResetData();
+            }
+            catch (Exception)
+            {
+            }
+        }
+    }
+}
