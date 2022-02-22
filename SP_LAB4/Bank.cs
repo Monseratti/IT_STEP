ea42676ed60de05cef391c701b23aa20bdaac1be
Roman Smal <98179242+Monseratti@users.noreply.github.com>
2022-02-22 20:23:23 +0200

    commit

---
+++ SP_LAB4/Bank.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace SP_LAB4
+{
+    [Serializable]
+    internal class Bank
+    {
+        public int money;
+        public int percent;
+        public string name;
+
+        public int GetMoney() { return money; }
+        public int GetPrecent() { return percent; }
+        public string GetName() { return name; }
+
+        public void SetMoney(int money) { this.money = money;          ThreadSer(); }
+        public void SetPercent(int percent) { this.percent = percent; ThreadSer(); }
+        public void SetName(string name) { this.name = name; ThreadSer(); }
+        public Bank(int m, int p, string n)
+        {
+            this.money = m;
+            this.percent = p;
+            this.name = n;
+        }
+        private void Serialize()
+        {
+            using (StreamWriter sw = new StreamWriter("log.json"))
+            {
+                JsonSerializer js = new JsonSerializer();
+                js.NullValueHandling = NullValueHandling.Ignore;
+                using (JsonTextWriter jsw = new JsonTextWriter(sw))
+                {
+                    js.Serialize(jsw, this);
+                }
+            }
+        }
+        private void ThreadSer()
+        {
+            ThreadStart ps = new ThreadStart(Serialize);
+            Thread thread = new Thread(ps);
+            thread.Start();
+            thread.Join();
+        }
+    }
+}
---
+++ SP_LAB4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SP_LAB4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //ParameterizedThreadStart threadStart = new ParameterizedThreadStart(MyMethod);
+            //Thread thread = new Thread(threadStart);
+            //thread.Start(new List<string>() { "Hello","world",});
+            Bank bank = new Bank(10,8,"H");
+            bank.SetMoney(100);
+            bank.SetPercent(100);
+            bank.SetName("SomeBank");
+            Console.ReadLine();
+        }
+        static void MyMethod(object a)
+        {
+            foreach (var item in (a as List<string>))
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
