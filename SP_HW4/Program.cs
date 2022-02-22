620a3c2080ccbb8fdd0afda80fe7b47f4e6f2ba0
Roman Smal <98179242+Monseratti@users.noreply.github.com>
2022-02-22 20:43:31 +0200

    commit

---
+++ SP_HW4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace SP_HW4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ThreadStart start = new ThreadStart(MyTimer);
+            Thread thread = new Thread(start);
+            thread.Start();
+            thread.Join();
+            Console.ReadLine();
+
+        }
+        static void MyTimer()
+        {
+            Console.WriteLine("Press any key");
+            DateTime date = DateTime.Now;
+            Console.ReadKey();
+            TimeSpan ts1 = DateTime.Now - date;
+            Console.WriteLine($"Time is {ts1.TotalMilliseconds} ms");
+        }
+    }
+}
