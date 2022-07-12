ea8f07e25081e5b7007a9cd08aa467744220cb40
Roman Smal <98179242+Monseratti@users.noreply.github.com>
2022-07-12 21:48:54 +0300

    commit all

---
+++ CW1-12_07-1/Car.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CW1_12_07_1
+{
+    public class Car
+    {
+        public bool IsLeft { get; set; }
+        public int Speed { get; set; }
+
+        public Car(bool isLeft, int speed)
+        {
+            IsLeft = isLeft;
+            Speed = speed;
+        }
+        public void Moove(PictureBox picture)
+        {
+            if (picture.InvokeRequired)
+            {
+                picture.Invoke(new Action(()=>Moove(picture)));
+            }
+            else
+            {
+                if (IsLeft)                picture.Left += Speed;
+                else                       picture.Left -= Speed;
+            }
+        }
+        public void Run(PictureBox picture, PictureBox box, object obj)
+        {
+            while (true)
+            {
+
+                while ((picture.Location.X + picture.Width) >= box.Location.X && picture.Location.X <= (box.Location.X + box.Width))
+                {
+                    lock (obj)
+                    {
+                        do
+                        {
+                            Moove(picture);
+                            Thread.Sleep(10);
+                        } while ((picture.Location.X + picture.Width) >= box.Location.X && picture.Location.X <= (box.Location.X + box.Width));
+                    }
+                }
+                Moove(picture);
+                Thread.Sleep(10);
+            }
+        }
+    }
+}
---
+++ CW1-12_07-1/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CW1_12_07_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            object key = new object();
+            Car[] cars = new Car[2]
+            {
+                new Car(true,10),
+                new Car(false,10)
+            };
+            Thread thread = new Thread(() => cars[0].Run(pictureBox1, pictureBox2, key)){ IsBackground = true};
+            Thread thread2 = new Thread(() => cars[1].Run(pictureBox3, pictureBox2, key)) { IsBackground = true};
+            thread.Start();
+            thread2.Start();
+
+        }
+    }
+}
