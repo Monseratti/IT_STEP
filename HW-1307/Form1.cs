4b51d695afefdcf773cdd6276939e70658dbf137
Roman Smal <98179242+Monseratti@users.noreply.github.com>
2022-07-13 21:40:24 +0300

    git commit

---
+++ HW-1307/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HW_1307
+{
+    public partial class Form1 : Form
+    {
+        string mutexName = "MyMutex";
+        Mutex mt = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                if (Mutex.OpenExisting(mutexName) != null)
+                {
+                    MessageBox.Show("This application is already open");
+                    this.Close();
+                }
+            }
+            catch (WaitHandleCannotBeOpenedException)
+            {
+                mt = new Mutex(true,mutexName);
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                mt.ReleaseMutex();
+            }
+            catch (Exception error)
+            {
+            }
+        }
+    }
+}
