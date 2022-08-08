4e3fe5c5c9ac829d614d6ed8cf9ab93bba8aab9b
Roman Smal <98179242+Monseratti@users.noreply.github.com>
2022-08-08 20:56:08 +0300

    commit

---
+++ CW_0808/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CW_0808
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IPAddress address = IPAddress.Parse("127.0.0.1");
+            IPEndPoint endPoint = new IPEndPoint(address, 8081);
+            Socket socket = new Socket(AddressFamily.InterNetwork, 
+                SocketType.Stream, ProtocolType.IP);
+            socket.Bind(endPoint);
+            socket.Listen(10);
+            try
+            {
+                while (true)
+                {
+                    Socket handler = socket.Accept();
+                    Console.WriteLine(handler.RemoteEndPoint.ToString());
+                    StringBuilder builder = new StringBuilder();
+                    int bytes = 0; 
+                    byte[] data = new byte[1024];
+
+                    do
+                    {
+                        bytes = handler.Receive(data);
+                        builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                    }
+                    while (handler.Available > 0);
+
+                    Console.WriteLine(DateTime.Now + ": " + builder.ToString());
+                    handler.Send(Encoding.Unicode.GetBytes($"{DateTime.Now}"));
+                    handler.Shutdown(SocketShutdown.Send);
+                    handler.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ CW_0808_Client/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CW_0808_Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IPAddress address = IPAddress.Parse("127.0.0.1");
+            IPEndPoint endPoint = new IPEndPoint(address, 8081);
+            Console.WriteLine($"{address}");
+            Console.WriteLine($"{endPoint}");
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
+            Console.Write("Input string: ");
+            string tmp = Console.ReadLine();
+            try
+            {
+                socket.Connect(endPoint);
+                if (socket.Connected)
+                {
+                    socket.Send(Encoding.Unicode.GetBytes(tmp));
+                    byte[] buff = new byte[1024];
+                    string str = "";
+                    int len = 0;
+                    do
+                    {
+                        len = socket.Receive(buff);
+                        str += Encoding.Unicode.GetString(buff, 0, len);
+                    } while (len > 0);
+                    Console.WriteLine($"Received message:\n{str}");
+                }
+                else
+                {
+                    Console.WriteLine("ERROR!");
+                }
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                // socket.Shutdown(SocketShutdown.Both);
+            }
+            Console.ReadKey();
+        }
+    }
+}
