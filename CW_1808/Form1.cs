1ae2802a3c62c20bcceb807af44ded268de06140
Roman Smal <98179242+Monseratti@users.noreply.github.com>
2022-08-18 20:59:28 +0300

    commit

---
+++ CW_1808/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CW_1808
+{
+    public partial class Form1 : Form
+    {
+        TcpListener listener;
+        public Form1()
+        {
+            InitializeComponent();
+            textBox1.Text = "127.0.0.1";
+            textBox2.Text = "8080";
+            FormClosing += Form1_FormClosing;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            listener?.Stop();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listener = new TcpListener(IPAddress.Parse(textBox1.Text), int.Parse(textBox2.Text));
+            listener.Start();
+            Thread thread = new Thread(Start) { IsBackground = true };
+            thread.Start();
+        }
+
+        private void Start()
+        {
+            while (true)
+            {
+                try
+                {
+                    TcpClient client = listener.AcceptTcpClient();
+                    using (StreamReader sr = new StreamReader(client.GetStream(), Encoding.Unicode))
+                    {
+                        string s = sr.ReadLine();
+                        Action action = () =>
+                        {
+                            listBox1.Items.Add(s);
+                        };
+                        Invoke(action);
+                        if (s.ToLower() == "exit")
+                        {
+                            listener?.Stop();
+                            Close();
+                        }
+                    }
+                    client.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+
+            }
+        }
+    }
+}
---
+++ CW_1808_Client/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CW_1808_Client
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            textBox1.Text = "127.0.0.1";
+            textBox2.Text = "8080";
+            this.Cursor = Cursors.Default;
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TcpClient client = new TcpClient();
+            client.Connect(IPAddress.Parse(textBox1.Text), int.Parse(textBox2.Text));
+            using (NetworkStream ns = client.GetStream())
+            {
+                byte[] buffer = Encoding.Unicode.GetBytes(textBox3.Text);
+                ns.Write(buffer, 0, buffer.Length);
+            }
+            client.Close();
+            if (textBox3.Text.ToLower() == "exit")
+            {
+                Close();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
